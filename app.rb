ab52dbbad4fe256646fdffedceb0c1155df98220
Aaron Chu <aaronchu357@gmail.com>
2019-05-12 16:26:03 -0400

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    'Hello, World!'
+  end
+
+  get '/name' do
+    "My name is Aaron Chu"
+  end
+
+  get '/hometown' do
+    "My hometown is New York"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is All The Above - Maino"
+  end
 end
